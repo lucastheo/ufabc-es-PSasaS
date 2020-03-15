3855d8e9c55dfa9a89573cd762dba55d2f36cf36
Arthur <arthur.h.f13@gmail.com>
2020-03-15 18:45:28 -0300

    updated teste.py

--- teste.py
+++ teste.py
@@ -1,14 +1,18 @@
+import time
 def criaModeloTeste(diagrama, requisitos):
     return "Modelagem Criada"
 
 #teste unitario
 
 def testeUnitario(codigo, modelo):
+    erro = False
     if (!erro):
         return 1
     return 0
 
 def testeCompleto(construcao, lista_todos_requisitos):
+    print("\nCompilando código...")
+    time.sleep(25)
     testes = []
     for i in range(nMetodos):
         testes.append(criaModeloTeste(diagrama, lista_todos_requisitos))
